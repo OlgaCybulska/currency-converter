f9322ae1b02bb5952f8c7c5b64af770d958250b2
Olga Cybulska <olgacybulska7@wp.pl>
2023-09-08 14:43:22 +0200

    JS funkction refactoring

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
-let amountElement = document.querySelector(".js-PLN")
-let exchangeElement = document.querySelector(".js-EUR")
-let formElement = document.querySelector(".js-form")
-let resultElement = document.querySelector(".js-PLNEUR")
 
 
-formElement.addEventListener("submit", (event) => {
+{
+const calculateResult = (amount, exchange) => {
+    const amountElement = document.querySelector(".js-PLN")
+    const exchangeElement = document.querySelector(".js-EUR")
+
+    return amount / exchange;
+
+};
+
+const onFormSubmit = (event) => {
     event.preventDefault();
 
-    let amount = amountElement.value;
-    let exchange = exchangeElement.value;
+    const amountElement = document.querySelector(".js-PLN")
+    const exchangeElement = document.querySelector(".js-EUR")
+    const resultElement = document.querySelector(".js-PLNEUR")
+
+    const amount = amountElement.value;
+    const exchange = exchangeElement.value;
+    
+
+    
+    const result = calculateResult(amount, exchange);
+
+    resultElement.innerText = result.toFixed(0)
+};
 
 
-    let result = amount / exchange;
+const init = () => {
+    const formElement = document.querySelector(".js-form")
 
+    formElement.addEventListener("submit", onFormSubmit);
 
-    resultElement.innerText  = result.toFixed(0);
+};
 
-});
+init();
+}
